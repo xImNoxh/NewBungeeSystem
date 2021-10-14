3abd3f2f8d646469ba3c47bd3f75462645dce188
xImNoxh <63975107+xImNoxh@users.noreply.github.com>
2021-10-14 18:57:14 +0200

    Edit BungeeSystem.java

--- src/main/java/de/kyoro/bansystem/BungeeSystem.java
+++ src/main/java/de/kyoro/bansystem/BungeeSystem.java
@@ -1,7 +1,10 @@
 package de.kyoro.bansystem;
 
+import de.kyoro.bansystem.config.Config;
+import de.kyoro.bansystem.config.ConfigReader;
 import de.kyoro.bansystem.database.MySQL;
 import lombok.Getter;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 @Getter
@@ -9,17 +12,20 @@ public class BungeeSystem extends Plugin {
 
     private static BungeeSystem instance;
     private static MySQL mySQL;
+    private static Config config = new ConfigReader().readConfig();
 
     @Override
     public void onEnable() {
         instance = this;
+        ProxyServer.getInstance().getConsole().sendMessage(config.getPrefix() + "§7Das §a§lBungeeSystem §7wurde erfolgreich §a§lgestartet§7.");
         mySQL = new MySQL();
         mySQL.connect();
     }
 
     @Override
     public void onDisable() {
-
+        ProxyServer.getInstance().getConsole().sendMessage(config.getPrefix() + "§7Das §a§lBungeeSystem §7wurde erfolgreich §c§lgestoppt§7.");
+        mySQL.disconnect();
     }
 
     public static BungeeSystem getInstance() {
